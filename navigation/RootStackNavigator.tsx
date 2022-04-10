2a0f56ecf421459fdc4dde75e6091418db9d9b49
tamertokbaev <tokbaev201536@gmail.com>
2022-04-10 16:04:15 +0600

    Navigation boilerplate

---
+++ navigation/RootStackNavigator.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
+
+const Stack = createNativeStackNavigator()
+
+const RootStackNavigator: React.FC = () => {
+
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen}/>
+    </Stack.Navigator>
+  )
+}
+
+export default RootStackNavigator
